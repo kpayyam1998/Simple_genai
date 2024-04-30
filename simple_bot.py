a294acf402ae318a13ffa29deef447390464dd0f
kpayyam1998 <karuppasamy12041998@gmail.com>
2024-04-30 09:55:05 +0530

    Code

---
+++ simple_bot.py
@@ -0,0 +1,24 @@
+import os
+from dotenv import load_dotenv
+import streamlit as st
+
+from langchain.prompts import PromptTemplate
+from langchain.llms import OpenAI
+from langchain.chains import LLMChain
+load_dotenv()
+
+key=os.getenv("OPENAI_API_KEY")
+llm=OpenAI(openai_api_key=key)
+st.title("Simple Bot which gives the product company names")
+p_name=st.text_input("Enter product name")
+if p_name:
+    prompt=PromptTemplate.from_template("Give me 5 unique company name which makes products {product_name}?")
+
+    llmchain=LLMChain(llm=llm,prompt=prompt)
+
+    st.write(llmchain.run(p_name))
+
+
+
+
+
